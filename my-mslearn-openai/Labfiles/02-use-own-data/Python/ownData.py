c760589e820f67cacf6fd5e44c3303a067533881
Anya Chaliotis <chq-anyac@corp.expeditors.com>
2025-07-21 13:20:10 -0700

    Copy of myExercise 'MSLearn Implement RAG with Azure OpenAI models' - deploy an Azure OpenAI model, connect sample data, and generate responses based on the sample data; W AzureAI; successful RAG including 2 more pdf for UA cities

---
+++ my-mslearn-openai/Labfiles/02-use-own-data/Python/ownData.py
@@ -0,0 +1,53 @@
+import os
+import openai
+import dotenv
+
+## Flag to show citations
+showCitations = False
+
+dotenv.load_dotenv()
+
+endpoint = os.environ.get("AZURE_OAI_ENDPOINT")
+api_key = os.environ.get("AZURE_OAI_KEY")
+deployment = os.environ.get("AZURE_OAI_DEPLOYMENT")
+
+
+client = openai.AzureOpenAI(
+    azure_endpoint=endpoint,
+    api_key=api_key,
+    api_version="2024-02-01",
+)
+
+# Configure your data source
+
+text = input('\nEnter a question:\n')
+    
+completion = client.chat.completions.create(
+    model=deployment,
+    messages=[
+        {
+            "role": "user",
+            "content": text,
+        },
+    ],
+    extra_body={
+        "data_sources":[
+            {
+                "type": "azure_search",
+                "parameters": {
+                    "endpoint": os.environ["AZURE_SEARCH_ENDPOINT"],
+                    "index_name": os.environ["AZURE_SEARCH_INDEX"],
+                    "authentication": {
+                        "type": "api_key",
+                        "key": os.environ["AZURE_SEARCH_KEY"],
+                    }
+                }
+            }
+        ],
+    }
+)
+
+print(completion.choices[0].message.content)
+
+if showCitations:
+    print(f"\n{completion.choices[0].message.context}")
